21dde46e589a346402cdc9b34b61d6516c9bf2fb
namePlayer <r06sollfrank@gmail.com>
2020-09-15 19:10:01 +0200

    Github Project

--- inc/database.inc.php
+++ inc/database.inc.php
@@ -1,3 +1,2 @@
 <?php
-$conn = new PDO("mysql:host=localhost;dbname=zap404568_pommdev", "zap404568_pdev", "8caE8*8zK8ts7&a5");
-// $conn = new PDO("mysql:host=localhost;dbname=pommesunity", "root", "");
+$conn = new PDO("mysql:host=localhost;dbname=pommesunity", "root", "");
