39fff9d47e03dbe906d8263fc7c2bbbe56e07993
namePlayer <r06sollfrank@gmail.com>
2020-09-15 19:08:22 +0200

    Beta System Settings

--- inc/database.inc.php
+++ inc/database.inc.php
@@ -1,2 +1,3 @@
 <?php
-$conn = new PDO("mysql:host=localhost;dbname=pommesunity", "root", "");
+$conn = new PDO("mysql:host=localhost;dbname=zap404568_pommdev", "zap404568_pdev", "8caE8*8zK8ts7&a5");
+// $conn = new PDO("mysql:host=localhost;dbname=pommesunity", "root", "");
